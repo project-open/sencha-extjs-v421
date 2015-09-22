eac8b0ff4ff823f73c6c17ef1d4c322f4a632943
Frank Bergmann <frank.bergmann@project-open.com>
2015-09-22 12:50:17 +0000

    - changed comment

---
+++ sql/postgresql/sencha-extjs-v421-create.sql
@@ -0,0 +1,14 @@
+-- /packages/sencha-extjs-v421/sql/postgresql/sencha-extjs-v421-create.sql
+--
+-- Copyright (c) 2015 ]project-open[
+--
+-- All rights reserved. Please check
+-- http://www.project-open.com/license/ for details.
+--
+-- @author frank.bergmann@project-open.com
+
+-- Nothing to do.
+-- Some TCL portlets will check for the presence of this package
+
+
+
